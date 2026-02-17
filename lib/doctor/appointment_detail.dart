fd471297e5f3a1fde6e3ba29f7889eff59c113df
Aadit Antony <aaditantony1112@gmail.com>
2026-02-17 09:13:39 +0530

    the folder structure and rest of the doctor and the user is done

--- lib/doctor/appointment_detail.dart
+++ lib/doctor/appointment_detail.dart
@@ -1,4 +1,4 @@
-import 'package:care_connect/refer_patient_page.dart';
+import 'package:care_connect/doctor/refer_patient_page.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:url_launcher/url_launcher.dart';
